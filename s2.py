d9676418b6542ddd7cfe02e04a9bdc22863e5c61
PapayaMilk <838606269@qq.com>
2019-02-25 18:05:36 +0800

    生成文件

--- s2.py
+++ s2.py
@@ -1,2 +1,34 @@
-a = "fsijofosj"
-print(a[-2:])
+# a = "fsijofosj"
+# print(a[-2:])
+
+# a = range(9,-1,-3)
+# print(list(a))
+
+# lst = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 32769, 65536, 4294967296]
+# di = dict()
+# for i in lst:
+#     if len(str(i)) in di.keys():
+#         di[len(str(i))].append(i)
+#     else:
+#         di[len(str(i))] = [i]
+# print(di)
+# from functools import reduce
+# lst = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# cst = []
+# cst.extend(lst)
+# print(cst)
+
+# print(reduce(lambda x, y: x+y, lst))
+# print(lst[0] + lst[1] + lst[2])
+
+# lst = [7, -8, 5, 4, 0, -2, -5]
+
+# lis = ['a', 'b', 'c', 'd', 'e']
+# print(lis[10:])
+
+# print(2**4)
+
+# for i in range(1, 10):
+#     for j in range(1, i+1):
+#         print('%s * %s = %s' % (i, j, i * j))
+
